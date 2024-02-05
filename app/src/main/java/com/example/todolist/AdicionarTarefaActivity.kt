109bda7d4078ccd9b29c3289250cd934e306fd77
gus <gustavohenrique.cns@gmail.com>
2024-02-05 20:50:09 -0300

    Minor changes e Activity add tarefa criada

--- app/src/main/java/com/example/todolist/AdicionarTarefaActivity.kt
+++ app/src/main/java/com/example/todolist/AdicionarTarefaActivity.kt
@@ -1,24 +1,19 @@
 package com.example.todolist
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityAdicionarTarefaBinding
 import com.example.todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class AdicionarTarefaActivity : AppCompatActivity() {
+
 
     private val biding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityAdicionarTarefaBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(biding.root)
-
-        biding.fabAdicionar.setOnClickListener {
-            val intent = Intent(this, AdicionarTarefaActivity::class.java)
-            startActivity(intent)
-        }
-
-
     }
 }
