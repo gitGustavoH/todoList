d33b4320101f936551e128660c50542e892ff40e
gus <gustavohenrique.cns@gmail.com>
2024-02-06 21:48:54 -0300

    Listando tarefas (bug)

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.todolist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.todolist.database.TarefaDAO
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.model.Tarefa
 
 class MainActivity : AppCompatActivity() {
 
     private val biding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var listaTarefa = emptyList<Tarefa>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(biding.root)
@@ -18,7 +24,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, AdicionarTarefaActivity::class.java)
             startActivity(intent)
         }
+    }
 
+    override fun onStart() {
+        super.onStart()
+      //  setContentView(biding.root)
+
+        val tarefaDAO = TarefaDAO(this)
+        listaTarefa = tarefaDAO.listar()
+
+        listaTarefa.forEach{ tarefa ->
+            Log.i("info_db", "${tarefa.descricao} \n")
+        }
 
     }
+
 }
--- app/src/main/java/com/example/todolist/database/TarefaDAO.kt
+++ app/src/main/java/com/example/todolist/database/TarefaDAO.kt
@@ -38,7 +38,32 @@ class TarefaDAO(context: Context) : ITarefaDAO {
     }
 
     override fun listar(): List<Tarefa> {
-        TODO("Not yet implemented")
+
+        val listaTarefas = mutableListOf<Tarefa>()
+
+        val sql = "SELECT ${DataBaseHelper.COLUNA_ID_TAREFA}, " +
+                "${DataBaseHelper.COLUNA_DESCRICAO}, " +
+                "strftime('%d/%m/%y %H:%M', ${DataBaseHelper.COLUNA_DATA_CADASTRO}) ${DataBaseHelper.COLUNA_DATA_CADASTRO} " +
+                "FROM ${DataBaseHelper.NOME_TABELA_TAREFAS}"
+
+        val cursor = leitura.rawQuery(sql, null)
+
+        val indiceId = cursor.getColumnIndex(DataBaseHelper.COLUNA_ID_TAREFA)
+        val indiceDescricao = cursor.getColumnIndex(DataBaseHelper.COLUNA_DESCRICAO)
+        val indiceData = cursor.getColumnIndex(DataBaseHelper.COLUNA_DATA_CADASTRO)
+
+        while (cursor.moveToNext()) {
+            val idTarefa = cursor.getInt(indiceId)
+            val descricao = cursor.getString(indiceDescricao)
+            val data = cursor.getString(indiceData)
+
+            listaTarefas.add(
+                Tarefa(idTarefa, descricao, data)
+            )
+        }
+
+        return listaTarefas
+
     }
 
 }
